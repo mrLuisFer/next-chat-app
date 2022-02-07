883d1044c6e54d30134e92194be83a79777a7782
mrLuisFer <mrluisfeer@gmail.com>
2022-02-07 17:13:16 -0600

    create UserContext with username and connected state

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,47 @@
+import {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+import { nanoid } from "nanoid";
+
+interface IUserCtx {
+  connected: boolean;
+  setConnected: Dispatch<SetStateAction<boolean>>;
+  username: string;
+  setUsername: Dispatch<SetStateAction<string>>;
+}
+
+const user: string = `User@${nanoid()}`;
+const userInitialValue: IUserCtx = {
+  connected: false,
+  setConnected: (b) => b,
+  username: user,
+  setUsername: (s) => s,
+};
+
+export const UserCtx = createContext(userInitialValue);
+
+export default function UserCtxProvider({ children }: { children: any }) {
+  const [connected, setConnected] = useState<boolean>(false);
+  const [username, setUsername] = useState<string>(user);
+
+  return (
+    <UserCtx.Provider
+      value={{ connected, setConnected, username, setUsername }}
+    >
+      {children}
+    </UserCtx.Provider>
+  );
+}
+
+export const useUserContext = (): IUserCtx => {
+  const ctx = useContext(UserCtx);
+  if (ctx === undefined) {
+    throw new Error("useConnectedCtx must be used within a ConnectedProvider");
+  }
+
+  return ctx;
+};
