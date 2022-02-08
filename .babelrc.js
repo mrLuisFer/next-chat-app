de1fe519c76f70d93d5018e3ecf995c1db4364f6
mrLuisFer <mrluisfeer@gmail.com>
2022-02-07 20:12:31 -0600

    remove custom babel config

--- .babelrc.js
+++
@@ -1,6 +0,0 @@
-const babelConfig = {
-  presets: [['next/babel', {'preset-react': {runtime: 'automatic'}}]],
-  plugins: ['babel-plugin-macros', ['styled-components', {ssr: true}]],
-};
-
-module.exports = babelConfig;
