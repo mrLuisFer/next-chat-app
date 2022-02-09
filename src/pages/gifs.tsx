e75d64e4c6f8e53fae7ceb4fe56eb1b0bd1ee2fc
mrLuisFer <mrluisfeer@gmail.com>
2022-02-08 22:06:25 -0600

    create future useful pages

---
+++ src/pages/gifs.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { Box } from "@chakra-ui/react";
+
+const Gifs: NextPage = () => {
+  return <Box>Hello</Box>;
+};
+
+export default Gifs;
---
+++ src/pages/user/[id].tsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import { NextPage } from "next";
+
+const UserById: NextPage = () => {
+  return <Box>Id</Box>;
+};
+
+export default UserById;
