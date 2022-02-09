d98a100cff9507678632487f290af8767d1d4927
mrLuisFer <mrluisfeer@gmail.com>
2022-02-08 22:35:21 -0600

    move leftsidebar into _app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,8 @@
 import type { NextPage } from "next";
-import { Grid } from "@chakra-ui/react";
 import Chat from "components/home/Chat";
-import LeftSidebar from "components/home/LeftSidebar";
 
 const Home: NextPage = () => {
-  return (
-    <Grid
-      width="100vw"
-      gridTemplateColumns="250px 1fr"
-      color="white"
-      gridGap="10px"
-    >
-      <LeftSidebar />
-      <Chat />
-    </Grid>
-  );
+  return <Chat />;
 };
 
 export default Home;
