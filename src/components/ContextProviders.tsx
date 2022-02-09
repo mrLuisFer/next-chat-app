c3aa9dfbab05fad770716fb9cdc748b72523994b
mrLuisFer <mrluisfeer@gmail.com>
2022-02-08 22:35:09 -0600

    move all context providers into a single component

---
+++ src/components/ContextProviders.tsx
@@ -0,0 +1,22 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import ChatContextProvider from "context/ChatContext";
+import UserContextProvider from "context/UserContext";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+});
+
+export default function ContextProviders({ children }: { children: any }) {
+  return (
+    <>
+      <ChakraProvider theme={theme}>
+        <ChatContextProvider>
+          <UserContextProvider>{children}</UserContextProvider>
+        </ChatContextProvider>
+      </ChakraProvider>
+    </>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,24 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import ChatContextProvider from "context/ChatContext";
-import UserContextProvider from "context/UserContext";
-
-const theme = extendTheme({
-  config: {
-    initialColorMode: "dark",
-    useSystemColorMode: false,
-  },
-});
+import { Grid, Box } from "@chakra-ui/react";
+import ContextProviders from "components/ContextProviders";
+import LeftSidebar from "components/home/LeftSidebar";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <ChatContextProvider>
-        <UserContextProvider>
+    <ContextProviders>
+      <Grid
+        width="100vw"
+        gridTemplateColumns="250px 1fr"
+        color="white"
+        gridGap="10px"
+      >
+        <LeftSidebar />
+        <Box p="2rem 0">
           <Component {...pageProps} />
-        </UserContextProvider>
-      </ChatContextProvider>
-    </ChakraProvider>
+        </Box>
+      </Grid>
+    </ContextProviders>
   );
 }
 
