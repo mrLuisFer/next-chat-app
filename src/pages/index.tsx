e045181f884f5d5ce907e2579fc8dc5faa7bbe5c
mrLuisFer <mrluisfeer@gmail.com>
2022-02-14 21:53:02 -0600

    add some auth validation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
+import { useState, useEffect } from 'react'
 import type { NextPage } from "next";
+import { useRouter } from 'next/router'
 import Chat from "components/home/Chat";
+import { useUserContext } from 'context/UserContext'
 
 const Home: NextPage = () => {
-  return <Chat />;
+  const [authorized, setAuthorized] = useState<boolean>(false)
+  const { username } = useUserContext()
+  const router = useRouter()
+
+  useEffect(() => {
+    console.log(`${username.length} ${authorized}`)
+    if (username.length > 2) {
+      setAuthorized(true)
+    } else {
+      router.push('/login')
+    }
+  }, [])
+
+  return (
+    <>{authorized && <Chat />}</>
+  )
 };
 
 export default Home;
