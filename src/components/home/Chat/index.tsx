143d6483e8ea4af823b7b2bf08c7e6b876406fdd
mrLuisFer <mrluisfeer@gmail.com>
2022-02-14 21:52:28 -0600

    add some custom padding

--- src/components/home/Chat/index.tsx
+++ src/components/home/Chat/index.tsx
@@ -5,7 +5,7 @@ import ChatHeader from "./ChatHeader";
 
 export default function Chat() {
   return (
-    <Box display="flex" flexFlow="column">
+    <Box display="flex" flexFlow="column" p="0 2rem 0 0.5rem">
       <ChatHeader />
       <ChatContent />
       <ChatInputs />
