b7eb33fa7559508393265e14cffa0421853482ff
mrLuisFer <mrluisfeer@gmail.com>
2022-02-19 22:40:29 -0600

    add more states for the user

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -5,13 +5,16 @@ import {
   useContext,
   useState,
 } from "react";
-import { nanoid } from "nanoid";
 
 interface IUserCtx {
   connected: boolean;
   setConnected: Dispatch<SetStateAction<boolean>>;
   username: string;
   setUsername: Dispatch<SetStateAction<string>>;
+  avatar: string;
+  setAvatar: Dispatch<SetStateAction<string>>
+  userId: string
+  setUserId: Dispatch<SetStateAction<string>>
 }
 
 // const user: string = `User@${nanoid()}`;
@@ -20,6 +23,10 @@ const userInitialValue: IUserCtx = {
   setConnected: (b) => b,
   username: '',
   setUsername: (s) => s,
+  avatar: '',
+  setAvatar: (s) => s,
+  userId: '',
+  setUserId: (s) => s
 };
 
 export const UserCtx = createContext(userInitialValue);
@@ -27,10 +34,12 @@ export const UserCtx = createContext(userInitialValue);
 export default function UserCtxProvider({ children }: { children: any }) {
   const [connected, setConnected] = useState<boolean>(false);
   const [username, setUsername] = useState<string>('');
+  const [avatar, setAvatar] = useState<string>('https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3')
+  const [userId, setUserId] = useState<string>('')
 
   return (
     <UserCtx.Provider
-      value={{ connected, setConnected, username, setUsername }}
+      value={{ connected, setConnected, username, setUsername, avatar, setAvatar, userId, setUserId }}
     >
       {children}
     </UserCtx.Provider>
