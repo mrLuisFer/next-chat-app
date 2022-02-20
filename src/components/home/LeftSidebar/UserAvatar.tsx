16dcabdda6b453f191961404dd881f6a98cf370c
mrLuisFer <mrluisfeer@gmail.com>
2022-02-19 22:40:37 -0600

    change static avatar

--- src/components/home/LeftSidebar/UserAvatar.tsx
+++ src/components/home/LeftSidebar/UserAvatar.tsx
@@ -1,17 +1,17 @@
-import { useUserContext } from "context/UserContext";
 import { Box, Heading, Image as ChakraImg } from "@chakra-ui/react";
 import Link from "next/link";
 import BtnContainer from "./BtnContainer";
+import {useUserContext} from 'context/UserContext'
 
 export default function UserAvatar() {
-  const { username } = useUserContext();
+  const { username, avatar, userId } = useUserContext();
 
   return (
-    <Link href={`/user/${1}`} passHref>
+    <Link href={`/user/${userId}`} passHref>
       <a>
         <BtnContainer>
           <ChakraImg
-            src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            src={avatar}
             w="60px"
             h="60px"
             borderRadius="50%"
