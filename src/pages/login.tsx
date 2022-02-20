6aed5cab0d13708d7afb3c89597f11e64dbd720f
mrLuisFer <mrluisfeer@gmail.com>
2022-02-19 19:41:26 -0600

    add basic login test for use firebase

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,6 +1,23 @@
 import type { NextPage } from "next";
+import { firestore } from 'lib/Firebase'
+import { collection, QueryDocumentSnapshot, DocumentData, query, where, limit, getDocs } from "@firebase/firestore";
 
 const Login: NextPage = () => {
+
+	const getUsers = async () => {
+		const usersCollection = collection(firestore, 'user')
+		const usersQuery = query(usersCollection, where('authorized', '==', true), limit(10));
+		const querySnapshot = await getDocs(usersQuery);
+		const result: QueryDocumentSnapshot<DocumentData>[] = [];
+		
+		querySnapshot.forEach((snapshot) => {
+			result.push(snapshot);
+		})
+		console.log(result);
+	}
+
+	getUsers()
+
 	return (
 		<div>
 			<p>Login</p>
