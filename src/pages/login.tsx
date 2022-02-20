7f89a1eec36c90b13ef76ad147ed4b157d3da2da
mrLuisFer <mrluisfeer@gmail.com>
2022-02-19 22:40:04 -0600

    change details with the login providers

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,26 +1,16 @@
 import type { NextPage } from "next";
-import { firestore, firebase } from 'lib/Firebase'
-import { useCollection } from 'react-firebase-hooks/firestore'
-import { collection } from 'firebase/firestore'
+import { firebase } from 'lib/Firebase'
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 const emailAuthProvider = firebase.auth.EmailAuthProvider.PROVIDER_ID
 const googleAuthProvider = firebase.auth.GoogleAuthProvider.PROVIDER_ID
 
+const uiConfig = {
+	signInSuccessUrl: '/',
+	signInOptions: [emailAuthProvider, googleAuthProvider],
+}
+
 const Login: NextPage = () => {
-	const uiConfig = {
-		signInSuccessUrl: '/',
-		signInOptions: [emailAuthProvider, googleAuthProvider],
-	}
-
-	const [value, loading, error] = useCollection(collection(firestore, 'users'), {})
-
-	console.log(loading)
-	if (!loading && value) {
-		value.docs.map((doc) => console.log(doc.data()));
-	}
-	console.log(error)
-
 	return (
 		<div>
 			<p>Login</p>
