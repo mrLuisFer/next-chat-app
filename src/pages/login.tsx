4f9914820941f23afe628f3722c755bfbda6b2d7
mrLuisFer <mrluisfeer@gmail.com>
2022-02-19 21:48:55 -0600

    change manual query for use hooks

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,26 +1,30 @@
 import type { NextPage } from "next";
-import { firestore } from 'lib/Firebase'
-import { collection, QueryDocumentSnapshot, DocumentData, query, where, limit, getDocs } from "@firebase/firestore";
+import { firestore, firebase } from 'lib/Firebase'
+import { useCollection } from 'react-firebase-hooks/firestore'
+import { collection } from 'firebase/firestore'
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+const emailAuthProvider = firebase.auth.EmailAuthProvider.PROVIDER_ID
+const googleAuthProvider = firebase.auth.GoogleAuthProvider.PROVIDER_ID
 
 const Login: NextPage = () => {
-
-	const getUsers = async () => {
-		const usersCollection = collection(firestore, 'user')
-		const usersQuery = query(usersCollection, where('authorized', '==', true), limit(10));
-		const querySnapshot = await getDocs(usersQuery);
-		const result: QueryDocumentSnapshot<DocumentData>[] = [];
-		
-		querySnapshot.forEach((snapshot) => {
-			result.push(snapshot);
-		})
-		console.log(result);
+	const uiConfig = {
+		signInSuccessUrl: '/',
+		signInOptions: [emailAuthProvider, googleAuthProvider],
 	}
 
-	getUsers()
+	const [value, loading, error] = useCollection(collection(firestore, 'users'), {})
+
+	console.log(loading)
+	if (!loading && value) {
+		value.docs.map((doc) => console.log(doc.data()));
+	}
+	console.log(error)
 
 	return (
 		<div>
 			<p>Login</p>
+			<StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
 		</div>
 	);
 };
