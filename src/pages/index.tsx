e855ce4f4ac2bcdc51e7e1d279c5fa40287a34ec
mrLuisFer <mrluisfeer@gmail.com>
2022-02-19 22:40:53 -0600

    add more info of the user, but just by firebase

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,39 @@
+import { useEffect } from 'react'
 import type { NextPage } from "next";
-import Chat from "components/home/Chat";
 import { useAuthState } from 'react-firebase-hooks/auth'
+import { Spinner } from '@chakra-ui/react'
 import { firebase } from 'lib/Firebase'
+import { useUserContext } from 'context/UserContext'
+import Chat from "components/home/Chat";
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
+
+  // Move into User Context file
   const auth: any = firebase.auth()
   const [user, loading, error] = useAuthState(auth)
+  const { setUsername, setAvatar, setUserId } = useUserContext()
+  const router = useRouter()
 
   console.log(loading, user, error)
+
+  useEffect(() => {
+    if (user) {
+      const username: string = user?.displayName as string
+      const avatar: string = user?.photoURL as string
+      const id: string = user?.uid as string
+      setUsername(username)
+      setAvatar(avatar)
+      setUserId(id)
+    } else {
+      router.push('/login')
+    }
+  }, [user])
+
   return (
-    <Chat />
+    <>
+      {loading ? <Spinner size="xl" speed="0.65s" thickness='3px' /> : <Chat />}
+    </>
   )
 };
 
