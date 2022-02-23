1cea19b4c26d457049f8ea943bf463a95e75675a
mrLuisFer <mrluisfeer@gmail.com>
2022-02-22 23:44:15 -0600

    add user context type into a single file

---
+++ src/types/IUserContext.d.ts
@@ -0,0 +1,12 @@
+export interface IUserCtx {
+	connected: boolean;
+	setConnected: Dispatch<SetStateAction<boolean>>;
+	username: string;
+	setUsername: Dispatch<SetStateAction<string>>;
+	avatar: string;
+	setAvatar: Dispatch<SetStateAction<string>>
+	userId: string
+	setUserId: Dispatch<SetStateAction<string>>
+	userLoading: boolean
+	setUserLoading: Dispatch<SetStateAction<boolean>>
+}
