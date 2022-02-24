d88248acc8deea93c252384128a54450e62af28f
mrLuisFer <mrluisfeer@gmail.com>
2022-02-23 23:13:23 -0600

    move user info into user context

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -4,18 +4,11 @@ import {
   SetStateAction,
   useContext,
   useState,
+  useEffect
 } from "react";
-
-interface IUserCtx {
-  connected: boolean;
-  setConnected: Dispatch<SetStateAction<boolean>>;
-  username: string;
-  setUsername: Dispatch<SetStateAction<string>>;
-  avatar: string;
-  setAvatar: Dispatch<SetStateAction<string>>
-  userId: string
-  setUserId: Dispatch<SetStateAction<string>>
-}
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { firebase } from 'lib/Firebase'
+import type { IUserCtx } from 'types/IUserContext'
 
 // const user: string = `User@${nanoid()}`;
 const userInitialValue: IUserCtx = {
@@ -26,7 +19,9 @@ const userInitialValue: IUserCtx = {
   avatar: '',
   setAvatar: (s) => s,
   userId: '',
-  setUserId: (s) => s
+  setUserId: (s) => s,
+  userLoading: false,
+  setUserLoading: (s) => s
 };
 
 export const UserCtx = createContext(userInitialValue);
@@ -36,10 +31,29 @@ export default function UserCtxProvider({ children }: { children: any }) {
   const [username, setUsername] = useState<string>('');
   const [avatar, setAvatar] = useState<string>('https://external-preview.redd.it/4PE-nlL_PdMD5PrFNLnjurHQ1QKPnCvg368LTDnfM-M.png?auto=webp&s=ff4c3fbc1cce1a1856cff36b5d2a40a6d02cc1c3')
   const [userId, setUserId] = useState<string>('')
+  const [userLoading, setUserLoading] = useState<boolean>(false)
+  const [userError, setUserError] = useState<any>(null)
+
+  const auth: any = firebase.auth()
+  const [user, loading, error] = useAuthState(auth)
+
+  useEffect(() => {
+    if (user) {
+      const username: string = user?.displayName as string
+      const avatar: string = user?.photoURL as string
+      const id: string = user?.uid as string
+
+      setUsername(username)
+      setAvatar(avatar)
+      setUserId(id)
+      setUserLoading(loading)
+      if (error) setUserError(error)
+    }
+  }, [user])
 
   return (
     <UserCtx.Provider
-      value={{ connected, setConnected, username, setUsername, avatar, setAvatar, userId, setUserId }}
+      value={{ connected, setConnected, username, setUsername, avatar, setAvatar, userId, setUserId, setUserLoading, userLoading }}
     >
       {children}
     </UserCtx.Provider>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,16 @@
-import { useEffect } from 'react'
 import type { NextPage } from "next";
-import { useAuthState } from 'react-firebase-hooks/auth'
 import { Spinner } from '@chakra-ui/react'
-import { firebase } from 'lib/Firebase'
 import { useUserContext } from 'context/UserContext'
-import Chat from "components/home/Chat";
+import Chat from "components/home/Chat"
 import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
-
   const router = useRouter()
-  // Move into User Context file
-  const auth: any = firebase.auth()
-  const [user, loading, error] = useAuthState(auth)
-  const { setUsername, setAvatar, setUserId } = useUserContext()
-
-  console.log(loading, user, error)
-
-  useEffect(() => {
-    if (user) {
-      const username: string = user?.displayName as string
-      const avatar: string = user?.photoURL as string
-      const id: string = user?.uid as string
-      setUsername(username)
-      setAvatar(avatar)
-      setUserId(id)
-    } else {
-      router.push('/login')
-    }
-  }, [user])
+  const { userLoading } = useUserContext()
 
   return (
     <>
-      {loading ? <Spinner size="xl" speed="0.65s" thickness='3px' /> : <Chat />}
+      {userLoading ? <Spinner size="xl" speed="0.65s" thickness='3px' /> : <Chat />}
     </>
   )
 };
