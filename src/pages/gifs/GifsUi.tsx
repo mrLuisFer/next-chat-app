20dec0bd61aee39b46972237f0efcee5102b4310
mrLuisFer <mrluisfeer@gmail.com>
2022-02-24 23:22:12 -0600

    move fetch func into Ui Component

--- src/pages/gifs/GifsUi.tsx
+++ src/pages/gifs/GifsUi.tsx
@@ -1,9 +1,37 @@
-import { Box } from '@chakra-ui/react'
+import { Box } from "@chakra-ui/react"
+import { useEffect, useState, useCallback } from "react"
+import SearchBar from "components/gifs/SearchBar"
+import GifsContent from "components/gifs/GifsContent"
+
+/*const apiKey = process.env.GIPHY_KEY*/
 
 export default function GifsUi() {
-	return (
-		<Box>
-			Gifs
-		</Box>
-	)
+  const [gifsQuery, setGifsQuery] = useState<string>("")
+  const [gifsData, setGifsData] = useState([])
+  const [gifsError, setGifsError] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const getGifs = useCallback(
+    async (giphyUrl: string) => {
+      setLoading(true)
+      const response: Response = await fetch(giphyUrl)
+      const data = await response.json()
+      if (data.data.length > 0) setGifsData(data.data)
+      else setGifsError(true)
+      setLoading(false)
+    },
+    [gifsQuery]
+  )
+
+  useEffect(() => {
+    const giphyUrl: string = `https://api.giphy.com/v1/gifs/search?api_key=p581czEQruojaMLjQ2dODC8vZ3UsSZRL&q=${gifsQuery}&limit=25&offset=0&rating=g&lang=en`
+    getGifs(giphyUrl)
+  }, [gifsQuery])
+
+  return (
+    <Box p="0 2rem 0 0.5rem">
+      <SearchBar setGifsQuery={setGifsQuery} gifsQuery={gifsQuery} />
+      {gifsQuery.length >= 3 ? <GifsContent gifsData={gifsData} loading={loading} /> : <Box>Search some gif</Box>}
+    </Box>
+  )
 }
--- src/pages/gifs/index.tsx
+++ src/pages/gifs/index.tsx
@@ -1,32 +1,11 @@
-import { useEffect, useState } from 'react'
-import type { NextPage } from "next";
-import { Box } from "@chakra-ui/react";
+import type { NextPage } from "next"
 import { useAuth } from "hooks/useAuth"
-import GifsUi from './GifsUi'
-
-const giphyApiKey: string = 'https://api.giphy.com/v1/gifs/search?api_key=p581czEQruojaMLjQ2dODC8vZ3UsSZRL&q=hacker&limit=25&offset=0&rating=g&lang=en'
+import GifsUi from "./GifsUi"
 
 const Gifs: NextPage = () => {
   const { authorized } = useAuth()
-  const [gifsData, setGifsData] = useState<any[]>([])
 
-  const getGifs = async () => {
-    const response: Response = await fetch(giphyApiKey)
-    const data = await response.json()
-    if (data.length > 0) setGifsData(data)
-    console.log(data)
-  }
+  return <>{authorized && <GifsUi />}</>
+}
 
-  useEffect(() => {
-    console.log(authorized)
-    getGifs()
-  }, [])
-
-  return (
-    <>
-      {authorized && <GifsUi />}
-    </>
-  )
-};
-
-export default Gifs;
+export default Gifs
