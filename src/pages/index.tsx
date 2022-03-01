895e0de68da7a8f1a0e28b231bb5f57faab01a7a
mrLuisFer <mrluisfeer@gmail.com>
2022-02-28 22:57:57 -0600

    remove unused router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,8 @@ import type { NextPage } from "next"
 import { Spinner } from "@chakra-ui/react"
 import { useUserContext } from "context/UserContext"
 import Chat from "components/home/Chat"
-import { useRouter } from "next/router"
 
 const Home: NextPage = () => {
-  const router = useRouter()
   const { userLoading } = useUserContext()
 
   return <>{userLoading ? <Spinner size="xl" speed="0.65s" thickness="3px" /> : <Chat />}</>
