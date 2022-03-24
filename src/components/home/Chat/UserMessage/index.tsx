95881ebe2e1031474bfd0424421fa062d27d37e9
mrLuisFer <mrluisfeer@gmail.com>
2022-03-24 13:25:23 -0600

    add modal functions and details on the user maessage

--- src/components/home/Chat/UserMessage/index.tsx
+++ src/components/home/Chat/UserMessage/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react"
 import { Box, Img, Text, ListItem } from "@chakra-ui/react"
+import Link from "next/link"
 import type { IMsg } from "types/IMsg"
 import { useUserContext } from "context/UserContext"
 import "./UserMessage.module.css"
@@ -8,18 +10,34 @@ interface UserMessageProps {
 }
 
 export default function UserMessage({ msg }: UserMessageProps) {
-  const { connected, avatar } = useUserContext()
+  const [showModal, setShowModal] = useState<boolean>(false)
+  const { connected, avatar, userId } = useUserContext()
+
+  const handleShowUserModal = () => {
+    setShowModal(true)
+  }
+
+  const handleHideModal = () => {
+    setShowModal(false)
+  }
 
   return (
-    <ListItem display="flex" alignItems="center">
-      <Box position="relative">
-        <Img src={avatar} alt={msg.user} borderRadius="50%" w="45px" h="45px" />
-        <div className={connected ? "connected" : ""} />
-      </Box>
-      <Box marginLeft="0.5rem">
-        <Text color="red.100">{msg.user}</Text>
-        <Text as="span">{msg.msg}</Text>
-      </Box>
-    </ListItem>
+    <>
+      <ListItem display="flex" alignItems="center">
+        <Box position="relative" onMouseEnter={() => handleShowUserModal()} onMouseLeave={() => handleHideModal()}>
+          <Link href={`/user/${userId}`}>
+            <a>
+              <Img src={avatar} alt={msg.user} borderRadius="50%" w="45px" h="45px" />
+              <div className={connected ? "connected" : ""} />
+            </a>
+          </Link>
+        </Box>
+        <Box marginLeft="0.5rem">
+          <Text color="red.100">{msg.user}</Text>
+          <Text as="span">{msg.msg}</Text>
+        </Box>
+      </ListItem>
+      {showModal && <Box>User modal</Box>}
+    </>
   )
 }
