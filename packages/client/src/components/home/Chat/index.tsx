18ede92c11b158d6ca47bba9fc1d67d923c5c9a8
mrLuisFer <mrluisfeer@gmail.com>
2022-04-21 13:45:55 -0500

    remove unused chat components and rename chat folder

--- packages/client/src/components/home/Chat/index.tsx
+++ packages/client/src/components/home/Chat/index.tsx
@@ -1,6 +1,4 @@
 import { Box } from "@chakra-ui/react"
-import ChatContent from "./ChatContent"
-import ChatInputs from "./ChatInput"
 import ChatHeader from "./ChatHeader"
 import dynamic from "next/dynamic"
 
@@ -10,8 +8,6 @@ export default function Chat() {
   return (
     <Box display="flex" flexFlow="column" p="0 2rem 0 0.5rem">
       <ChatHeader />
-      {/* <ChatContent />
-      <ChatInputs /> */}
       <AblyChatComponent />
     </Box>
   )
