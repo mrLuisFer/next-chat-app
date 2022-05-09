0815d879e4a32dc85a10c66c187b65c583f62733
mrLuisFer <mrluisfeer@gmail.com>
2022-05-09 00:20:23 -0500

    update server file and add socketio

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,30 @@
-import express from "express"
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
-const PORT = 8000
-app.listen(PORT, (): void => {
-  console.log(`Server running on Port: ${PORT}`)
-})
+const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+io.on("connection", (socket) => {
+  console.log(`Client ${socket.id} connected`);
+
+  const { roomId } = socket.handshake.query;
+  socket.join(roomId as any);
+
+  socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+    io.in(roomId as any).emit(NEW_CHAT_MESSAGE_EVENT, data);
+  });
+
+  socket.on("disconnect", (reason) => {
+    console.log(`Client disconnected 'cause: ${reason}`);
+  });
+});
+
+io.listen(8000);
