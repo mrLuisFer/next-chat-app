ec3457aff4998a29c64a5da6e09eb4837379aa42
mrLuisFer <mrluisfeer@gmail.com>
2022-05-09 00:19:57 -0500

    remove old ably config for create manually socketio server

--- packages/client/src/components/home/Chat/ChatContent.tsx
+++ packages/client/src/components/home/Chat/ChatContent.tsx
@@ -1,42 +1,20 @@
-import { useEffect } from "react"
-import * as io from "socket.io-client"
 import { nanoid } from "nanoid"
-import { useChatCtx } from "context/ChatContext"
-import type { IMsg } from "types"
-import { useUserContext } from "context/UserContext"
 import { Box, UnorderedList } from "@chakra-ui/react"
+import { useChat } from "hooks/useChat"
 import UserMessage from "./UserMessage"
 
-export default function ChatContent() {
-  const { chat, setChat } = useChatCtx()
-  const { setConnected } = useUserContext()
-
-  const baseUrl: string = process.env.BASE_URL!
-  const socket = io.connect(baseUrl, {
-    path: "/api/socket",
-  })
-  useEffect((): any => {
-    socket.on("connect", () => {
-      console.log("SOCKET CONNECTED!", socket.id)
-      setConnected(true)
-    })
-
-    socket.on("chat-message", (message: IMsg) => {
-      setChat((c: IMsg[]) => [...c, message])
-    })
-
-    if (socket) {
-      return () => {
-        setConnected(false)
-        socket.disconnect()
-      }
-    }
-  }, [])
+export default function ChatContent({ query }: any) {
+  const { messages } = useChat(query)
+  console.log(messages)
 
   return (
     <Box overflowY="auto" h="460px" className="chatContent">
       <UnorderedList styleType="none" display="flex" flexDirection="column" gap="1rem 0" margin="0">
-        {chat.length ? chat.map((msg) => <UserMessage key={nanoid()} msg={msg} />) : <div>No chat messages</div>}
+        {messages.length > 1 ? (
+          messages.map((msg) => <UserMessage key={nanoid()} msg={msg} />)
+        ) : (
+          <div>No chat messages</div>
+        )}
       </UnorderedList>
     </Box>
   )
--- packages/client/src/pages/api/ablyHandler.ts
+++
@@ -1,9 +0,0 @@
-import Ably from "ably/promises"
-import { NextApiRequest } from "next"
-import { NextApiResponseIo } from "types/NextApiResponse"
-
-export default async function handler(req: NextApiRequest, res: NextApiResponseIo) {
-  const client = new Ably.Realtime(process.env.ABLY_API_KEY || "")
-  const tokenRequestData = await client.auth.createTokenRequest({ clientId: "ably-nextjs-demo" })
-  res.status(200).json(tokenRequestData)
-}
