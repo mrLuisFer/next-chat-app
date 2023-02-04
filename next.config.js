6d1f428cf4f424315233d6d62e1946847c636f05
Luis Alvarez <luis.alvarez@nowports.com>
2023-02-04 16:51:54 -0600

    update environment variables

--- next.config.js
+++ next.config.js
@@ -10,6 +10,12 @@ const nextConfig = {
     config.experiments = { ...config.experiments, topLevelAwait: true }
     return config
   },
+  env: {
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY
+  }
 }
 
 module.exports = nextConfig
