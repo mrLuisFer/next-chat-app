80485a078ec16cbec8f2b3e23baa80c1fc185151
Luis Alvarez <luis.alvarez@nowports.com>
2023-02-04 16:24:12 -0600

    add environment files

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript',
+    'next',
+    'next/core-web-vitals',
+    'prettier'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+  },
+}
